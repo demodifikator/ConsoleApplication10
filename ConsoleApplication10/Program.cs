e0e3514c7d5b801502acf4276b843e4d457d84f4
Lenovo <Lenovo@DESKTOP-80UM70A>
2017-12-14 23:35:24 +0200

    Четность

--- ConsoleApplication10/Program.cs
+++ ConsoleApplication10/Program.cs
@@ -10,6 +10,31 @@ namespace ConsoleApplication10
     {
         static void Main(string[] args)
         {
+            byte n;
+            Console.Write("Введите размер массива: ");
+            n = byte.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            Console.WriteLine("Вводите элементы массива: ");
+            for (byte i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{i + 1}) ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Четные элементы массива: ");
+            for (byte i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                    Console.WriteLine(arr[i]);                
+            }
+
+            Console.Write("Нечетные элементы массива: ");
+            for (byte i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                    Console.WriteLine(arr[i]);
+            }
+
         }
     }
 }
